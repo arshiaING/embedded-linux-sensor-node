db6149770b3546281ee8cb534fb9a9dcaee3a96d
arshiaING <272900@usn.no>
2026-06-14 18:18:21 +0200

    Move fake sensor code into separate files

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,16 +3,9 @@
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
+#include "FakeSensor.hpp"
 
-double readFakeTemperature()
-{
-    // Simulate reading a temperature value from a sensor this is base on norwegian weather data in the summer months, which typically ranges from 10.0 to 20.0 degrees Celsius.
-    
-    double base = 10.0; // Base temperature
-    double randomPart = (rand() % 1000) / 100.0; // Simulate a random value between 0.0 and 10.0
-    return base + randomPart; // Simulate a temperature reading between 10.0 and 20.0 degrees Celsius
 
-}
 
 int main()
 {
--- docs/01-learning-log.md
+++ docs/01-learning-log.md
@@ -2,7 +2,7 @@
 
 The first fake sensor always returned the same temperature.
 
-I changed the function so that it now returns a random temperature between approximately 20 and 30 degrees.
+I changed the function so that it now returns a random temperature between approximately 10 and 20 degrees.
 
 I used `srand()` with the current time so that the program does not produce the same sequence every time it starts.
 
