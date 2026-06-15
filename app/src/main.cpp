220686f66a5f29632231874d3d5c838e02ad240c
arshiaING <272900@usn.no>
2026-06-15 04:04:31 +0200

    Add graceful shutdown with Ctrl+C

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,28 +1,42 @@
-#include <iostream>
-#include <thread>
 #include <chrono>
+#include <csignal>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <thread>
+
 #include "FakeSensor.hpp"
 
+volatile std::sig_atomic_t running = 1;
 
+void handleSignal(int signal)
+{
+    if (signal == SIGINT) {
+        running = 0;
+    }
+}
 
 int main()
 {
-    srand(time(nullptr)); // Seed the random number generator
+    srand(time(nullptr));
+
+    std::signal(SIGINT, handleSignal);
 
     std::cout << "Starting sensor prototype..." << std::endl;
-    std::cout << "Press Ctrl + C to stop the program." << std::endl; // Inform the user how to stop the program real sensor would run indefinitely, but for testing we will run it a few times and then exit with ctrl + c
-    while (true) {
+    std::cout << "Press Ctrl + C to stop the program." << std::endl;
+
+    while (running) {
         double temperature = readFakeTemperature();
 
-        std::cout << "Temperature: " << temperature << " C" << std::endl;
-        
+        std::cout << "Temperature: "
+                  << temperature
+                  << " C"
+                  << std::endl;
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    std::cout << "Program finished." << std::endl;
+    std::cout << "\nStopping sensor prototype..." << std::endl;
 
     return 0;
 }
